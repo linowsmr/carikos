52d83d2ed26a5dc353e498048cd07e26abbd26a9
Luwandino Wismar <luwandino@hotmail.com>
2017-03-21 18:10:43 +0700

    Penggabungan KMeans

--- application/controllers/pencarian.php
+++ application/controllers/pencarian.php
@@ -8,6 +8,7 @@ class Pencarian extends CI_Controller {
 	   	parent::__construct();
 	   	$this->load->model('model_kos','',TRUE);
 	   	$this->load->model('model_kamar','',TRUE);
+	   	$this->load->model('model_pencarian','',TRUE);
  	}
 
 	public function index()
@@ -38,6 +39,63 @@ class Pencarian extends CI_Controller {
 
 		$fasilitaskos = implode(",", $kos);
 		$fasilitaskamar = implode(",", $kamar);
+
+		$data['hasil'] = $this->model_pencarian->pencarian($minHarga, $maxHarga, $tipe, $fasilitaskos, $fasilitaskamar);
+
+		$point = array();
+		$dataPoint = array();
+
+		foreach($data['hasil'] as $row){
+			$latlong = substr($row->latLngKos, 1, -1);
+			$coord = explode(", ", $latlong);
+
+			array_push($point, $coord[0]);
+			array_push($point, $coord[1]);
+		}
+
+		$totalCoord = sizeof($data['hasil']);
+
+		for($i =0; $i < $totalCoord; $i++){
+			for($j =0; $j < 2; $j++){
+				$dataPoint[$i][$j] = array_shift($point);
+			}
+		}
+
+		// $dataPoint[0][0] = $point[0];
+		// $dataPoint[0][1] = $point[1];
+		// $dataPoint[1][0] = $point[2];
+		// $dataPoint[1][1] = $point[3];
+		
+		require_once "assets/KMeans/Space.php";
+		require_once "assets/KMeans/Point.php";
+		require_once "assets/KMeans/Cluster.php";
+
+		// create a 2-dimentions space
+		$space = new KMeans\Space(2);
+
+		// add points to space
+		foreach ($dataPoint as $coordinates)
+			$space->addPoint($coordinates);
+
+		// cluster these 50 points in 3 clusters
+		$clusters = $space->solve(2);
+
+		//var_dump($clusters);
+
+		// display the cluster centers and attached points
+		foreach ($clusters as $i => $cluster){
+			printf("----- Cluster %s (%f,%f): %d points -----", $i, $cluster[0], $cluster[1], count($cluster));
+			
+			foreach ($cluster as $j => $member){
+				$latlng = "($member[0], $member[1])";
+				$idKos = $this->model_pencarian->pencarian_by_latlng($latlng);
+				foreach($idKos as $row){
+					printf("$row->idKos");
+				}
+				//printf("$idKos");
+			}
+		}
+		
 	}
 	
 }
--- application/models/model_pencarian.php
+++ application/models/model_pencarian.php
@@ -1,8 +1,15 @@
 <?php
 class Model_Pencarian extends CI_Model {
- 	function fasilitas()
+ 	function pencarian($minHarga, $maxHarga, $tipe, $fasilitaskos, $fasilitaskamar)
  	{
- 		$query = "SELECT * FROM fasilitaskos";
+ 		$query = "SELECT k.latLngKos FROM kos k, kamar km, (SELECT DISTINCT km.idKamar FROM kos k, kos_fasilitaskos kf, kamar km, kamar_fasilitaskamar kmf WHERE k.idKos = kf.idKos AND k.idKos = km.idKos AND km.idKamar = kmf.idKamar AND km.hargaKamar >= $minHarga AND km.hargaKamar <= $maxHarga AND k.idTipekos = $tipe AND kf.idFasilitasKos IN ($fasilitaskos) AND kmf.idFasilitasKamar IN ($fasilitaskamar)) res WHERE k.idKos = km.idKos AND km.idKamar = res.idKamar";
+		$run = $this->db->query($query);
+		return $run->result();
+ 	}
+
+ 	function pencarian_by_latlng($latlng)
+ 	{
+ 		$query = "SELECT k.idKos FROM kos k WHERE k.latLngKos = '$latlng'";
 		$run = $this->db->query($query);
 		return $run->result();
  	}

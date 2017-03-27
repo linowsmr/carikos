28bb15d697c653ab6854057aaebb61ffc83f9250
Luwandino Wismar <luwandino@hotmail.com>
2017-03-27 21:48:25 +0700

    Query untuk Pembobotan

--- application/controllers/pencarian.php
+++ application/controllers/pencarian.php
@@ -7,6 +7,7 @@ class Pencarian extends CI_Controller {
  	{
 	   	parent::__construct();
 	   	$this->load->model('model_pencarian','',TRUE);
+	   	$this->load->model('model_cluster','',TRUE);
  	}
 
 	public function index()
@@ -38,6 +39,67 @@ class Pencarian extends CI_Controller {
 		$fasilitaskos = implode(",", $kos);
 		$fasilitaskamar = implode(",", $kamar);
 
+		$nilaiDestinasi = 0;
+
+		$cluster = $this->model_cluster->ambil_cluster();
+		foreach($cluster as $row){
+			$idCluster = $row->idCluster;
+			if($row->nilaiDestinasiCluster == 0){
+				$jarakMinimarket = $this->model_cluster->jarak_destinasi($idCluster, 1);
+				$jarakSupermarket = $this->model_cluster->jarak_destinasi($idCluster, 2);
+
+				if($jarakMinimarket < $jarakSupermarket){
+					$jarakMarket = $jarakMinimarket;
+				} else if ($jarakMinimarket > $jarakSupermarket) {
+					$jarakMarket = $jarakSupermarket;
+				} else if ($jarakMinimarket == $jarakSupermarket){
+					$jarakMarket = $jarakMinimarket;
+				}
+
+				//var_dump($jarakMarket);
+				foreach($jarakMarket as $row){
+					$jarakFinalMarket = $row->jarakDestinasi;
+					$bobotMarket = 0.22;
+
+					if($jarakFinalMarket <= 2)
+						$nilaiDestinasi = $nilaiDestinasi + 100*$bobotMarket;
+					else if($jarakFinalMarket > 2 && $jarakFinalMarket <=3)
+						$nilaiDestinasi = $nilaiDestinasi + 75*$bobotMarket;
+					else if($jarakFinalMarket > 3 && $jarakFinalMarket <=4)
+						$nilaiDestinasi = $nilaiDestinasi + 50*$bobotMarket;
+					else if($jarakFinalMarket > 4 && $jarakFinalMarket <=5)
+						$nilaiDestinasi = $nilaiDestinasi + 25*$bobotMarket;
+					else if($jarakFinalMarket > 5)
+						$nilaiDestinasi = $nilaiDestinasi + 0*$bobotMarket;
+					
+					//echo "ID Cluster = $idCluster, Nilai Sementara = $nilaiDestinasi <br>";
+				}
+
+				$jarakMasjid = $this->model_cluster->jarak_destinasi($idCluster, 3);
+
+				foreach($jarakMasjid as $row){
+					$jarakFinalMasjid = $row->jarakDestinasi;
+					$bobotTempatIbadah = 0.02;
+
+					if($jarakFinalMasjid <= 2)
+						$nilaiDestinasi = $nilaiDestinasi + 100*$bobotTempatIbadah;
+					else if($jarakFinalMasjid > 2 && $jarakFinalMarket <=3)
+						$nilaiDestinasi = $nilaiDestinasi + 75*$bobotTempatIbadah;
+					else if($jarakFinalMasjid > 3 && $jarakFinalMarket <=4)
+						$nilaiDestinasi = $nilaiDestinasi + 50*$bobotTempatIbadah;
+					else if($jarakFinalMasjid > 4 && $jarakFinalMarket <=5)
+						$nilaiDestinasi = $nilaiDestinasi + 25*$bobotTempatIbadah;
+					else if($jarakFinalMasjid > 5)
+						$nilaiDestinasi = $nilaiDestinasi + 0*$bobotTempatIbadah;
+					
+					echo "ID Cluster = $idCluster, Nilai Sementara = $nilaiDestinasi <br>";
+				}
+
+			}
+			$nilaiDestinasi = 0;
+		}
+
+		exit();
 		$data['hasil'] = $this->model_pencarian->pencarian($minHarga, $maxHarga, $tipe, $fasilitaskos, $fasilitaskamar);
 		//var_dump($data['hasil']);
 		$this->load->view('template/header');
--- application/models/model_cluster.php
+++ application/models/model_cluster.php
@@ -57,4 +57,39 @@ class Model_Cluster extends CI_Model {
 
 	   	$run = $this->db->insert('cluster_destinasi', $data);
  	}
+
+ 	function ambil_cluster()
+ 	{
+ 		$query = "SELECT * FROM cluster";
+		$run = $this->db->query($query);
+		return $run->result();
+ 	}
+
+ 	function jarak_destinasi($idCluster, $idDestinasi)
+ 	{
+ 		$query = "SELECT DISTINCT cd.jarakDestinasi FROM cluster_destinasi cd WHERE cd.idCluster = $idCluster AND cd.idDestinasi = $idDestinasi ORDER BY cd.idClusterDestinasi DESC LIMIT 1";
+		$run = $this->db->query($query);
+		return $run->result();
+ 	}
+
+ 	function luas_parkiran($idKos)
+ 	{
+ 		$query = "SELECT k.idParkiranKos FROM kos k WHERE k.idKos = $idKos";
+		$run = $this->db->query($query);
+		return $run->result();
+ 	}
+
+ 	function penjaga_kos($idKos)
+ 	{
+ 		$query = "SELECT kf.idKosFasilitasKos FROM kos k, kos_fasilitaskos kf WHERE k.idKos = kf.idKos AND kf.idKos = $idKos AND kf.idFasilitasKos = 4";
+		$run = $this->db->query($query);
+		return $run->num_rows();
+ 	}
+
+ 	function fasilitas_lengkap($idKamar)
+ 	{
+ 		$query = "SELECT (count(kmf.idKamarFasilitasKamar) + res2.fasilitasDua) as fasilitasTiga FROM kamar_fasilitaskamar kmf, (SELECT (count(kmf.idKamarFasilitasKamar) + res.fasilitasSatu) as fasilitasDua FROM kamar_fasilitaskamar kmf, (SELECT count(kmf.idKamarFasilitasKamar) as fasilitasSatu FROM kamar_fasilitaskamar kmf WHERE kmf.idKamar = $idKamar AND kmf.idFasilitasKamar = 1) res WHERE kmf.idKamar = $idKamar AND kmf.idFasilitasKamar = 2) res2 WHERE kmf.idKamar = $idKamar AND kmf.idFasilitasKamar = 3";
+		$run = $this->db->query($query);
+		return $run->result();
+ 	}
 }
